bd6067076529471c0030d7578eb8fe88abaf6cc8
Victor Bourgouin <victor.bourgouin@hotmail.fr>
2021-11-10 14:28:20 +0100

    exo 2 done

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,8 @@
 class Restaurant < ApplicationRecord
+  has_many :reviews, dependent: :destroy
+
+  categories = ["chinese", "italian", "japanese", "french", "belgian"]
+  validates :name, presence: true
+  validates :address, presence: true
+  validates :category, inclusion: { in: categories }, presence: true
 end
